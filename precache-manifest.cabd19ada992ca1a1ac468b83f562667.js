423d93f21c0fab0eb051325a3492b403eef48506
Óscar Plaza <oscarplate@gmail.com>
2020-09-12 22:00:07 +0200

    Updates

---
+++ precache-manifest.cabd19ada992ca1a1ac468b83f562667.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319e3a1d7cf6b3ff2ff8bdf8069848e5",
+    "url": "/another-react-website/index.html"
+  },
+  {
+    "revision": "1fa4c09c121b1a4c3803",
+    "url": "/another-react-website/static/css/main.d552351a.chunk.css"
+  },
+  {
+    "revision": "f059ccace5a3d1f5d094",
+    "url": "/another-react-website/static/js/2.f7248e7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/another-react-website/static/js/2.f7248e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa4c09c121b1a4c3803",
+    "url": "/another-react-website/static/js/main.ecd0a772.chunk.js"
+  },
+  {
+    "revision": "f9dc421a1be685802e7f",
+    "url": "/another-react-website/static/js/runtime-main.720ae886.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/another-react-website/static/media/logo.ee7cd8ed.svg"
+  }
+]);
