e07f5ca74aa1bea05bfbb3ba7fde28efc19d7363
Óscar Plaza <34540141+AllRacs@users.noreply.github.com>
2020-09-12 09:35:34 +0000

    initial set up

---
+++ my-site/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My first React App
+        </p>
+        <Hello />
+      </header>
+    </div>
+  );
+}
+
+function Hello() {
+  return(
+    <div className="Hello">
+      <span>Hello Internet</span>
+    </div>
+  );
+}
+
+export default App;
