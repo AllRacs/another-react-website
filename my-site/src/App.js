17ed3c514147eec8aedb3c5b3e52654081904bf2
Óscar Plaza <oscarplate@gmail.com>
2020-09-14 16:18:17 +0200

    href code

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -6,8 +6,11 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>My first React App</p>
+                <div>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>My first React App</p>
+                </div>
+                <a href="https://github.com/AllRacs/another-react-website" target="blank" className="text-white">&#60;/github&#62;</a>
             </header>
         </div>
     );
